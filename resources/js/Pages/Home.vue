50a3d3384df71d08ad8d66626690bfdff2744b61
Md Sohan <sohanmd381@gmail.com>
2026-03-04 22:38:23 +0600

    Scroll Preservation

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 <template>
     <Head :title="` | ${$page.component}`" />
-    <h1 class="text-3xl font-bold text-center mt-10"> {{ $page.props.auth.user }} </h1>
+    <Link class="mt-[1400px] block" :href="route('home')" preserve-scroll>Refresh</Link>
 </template>
